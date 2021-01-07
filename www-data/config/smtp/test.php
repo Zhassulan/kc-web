b5f415d673120cfcd9e8f513281d87c7859dc009
Developer.KZ <developer.kz@gmail.com>
2021-01-07 15:28:16 +0600

    func, mail

--- www-data/config/smtp/test.php
+++ www-data/config/smtp/test.php
@@ -1,11 +1,11 @@
 <?php
-$site['from_name'] = 'do-not-reply@example.com';
-$site['from_email'] = 'do-not-reply@example.com';
+$site['from_name'] = 'do-not-reply@ets.kz';
+$site['from_email'] = 'do-not-reply@ets.kz';
 $site['smtp_mode'] = 'enabled';
-$site['smtp_host'] = 'localhost';
+$site['smtp_host'] = 'mail.cloud24.kz';
 $site['smtp_port'] = 25;
-$site['smtp_username'] = 'admin@example.com';
-$site['smtp_password'] = 'admin';
+$site['smtp_username'] = 'do-not-reply@ets-tender.kz';
+$site['smtp_password'] = 'M2N9f8xPZ!';
 
 $mail['kc_operator'] = 'ztokbayev@gmail.com';
 $mail['kc_operator1'] = 'ztokbayev@gmail.com';
