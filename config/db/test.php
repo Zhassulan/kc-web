225287526c4e450b5839dae1759d7884da84e852
Developer.KZ <developer.kz@gmail.com>
2021-01-09 15:02:45 +0600

    make config external

---
+++ config/db/test.php
@@ -0,0 +1,15 @@
+<?php
+$db['server'] = 'localhost:8083';
+$db['user'] = 'kc';
+$db['pwd'] = 'WZcjCH6f';
+$db['db'] = 'db_kc';
+
+$db1['server'] = 'localhost:8083';
+$db1['user'] = 'kc';
+$db1['pwd'] = 'WZcjCH6f';
+$db1['db'] = 'db_kc1';
+
+$msdb['db'] = 'mssql_db';
+$msdb['user'] = 'ztokbayev';
+$msdb['pwd'] = 'SqlPwd2016';
+
---
+++ config/smtp/test.php
@@ -0,0 +1,12 @@
+<?php
+$site['from_name'] = 'do-not-reply@ets.kz';
+$site['from_email'] = 'do-not-reply@ets.kz';
+$site['smtp_mode'] = 'enabled';
+$site['smtp_host'] = 'mail.cloud24.kz';
+$site['smtp_port'] = 25;
+$site['smtp_username'] = 'do-not-reply@ets.kz';
+$site['smtp_password'] = 'M2N9f8xPZ!';
+
+$mail['kc_operator'] = 'dev@ets.kz';
+$mail['kc_operator1'] = 'dev@ets.kz';
+$mail['members_operator'] = 'dev@ets.kz';
