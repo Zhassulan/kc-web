bf24460b975426fc8980049b8d0b9e7c065b71f3
Developer.KZ <developer.kz@gmail.com>
2021-01-09 14:28:22 +0600

    email, C01

--- www-data/config/smtp/prod.php
+++ www-data/config/smtp/prod.php
@@ -4,7 +4,7 @@ $site['from_email'] = 'do-not-reply@ets.kz';
 $site['smtp_mode'] = 'enabled';
 $site['smtp_host'] = 'mail.cloud24.kz';
 $site['smtp_port'] = 25;
-$site['smtp_username'] = 'do-not-reply@ets-tender.kz';
+$site['smtp_username'] = 'do-not-reply@ets.kz';
 $site['smtp_password'] = 'M2N9f8xPZ!';
 
 $mail['kc_operator'] = 'yugay@ets.kz';
--- www-data/config/smtp/test.php
+++ www-data/config/smtp/test.php
@@ -4,9 +4,9 @@ $site['from_email'] = 'do-not-reply@ets.kz';
 $site['smtp_mode'] = 'enabled';
 $site['smtp_host'] = 'mail.cloud24.kz';
 $site['smtp_port'] = 25;
-$site['smtp_username'] = 'do-not-reply@ets-tender.kz';
+$site['smtp_username'] = 'do-not-reply@ets.kz';
 $site['smtp_password'] = 'M2N9f8xPZ!';
 
-$mail['kc_operator'] = 'ztokbayev@gmail.com';
-$mail['kc_operator1'] = 'ztokbayev@gmail.com';
-$mail['members_operator'] = 'ztokbayev@gmail.com';
+$mail['kc_operator'] = 'dev@ets.kz';
+$mail['kc_operator1'] = 'dev@ets.kz';
+$mail['members_operator'] = 'dev@ets.kz';
